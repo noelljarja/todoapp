83c423ee5b1d0fe58e0a83976562b40e6b121512
noelljarja <35272905+noelljarja@users.noreply.github.com>
2018-06-29 16:06:21 +0200

    Added patch todos

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 var {mongoose}=require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -53,6 +54,27 @@ app.delete('/todos/:id',function(req,res){
         res.status(404).send(e);
     })
 });
+app.patch('/todos/:id', function(req,res){
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt=new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id,{$set: body},{new:true}).then(function(todo){
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(function(e){
+        res.status(400).send();
+    })
+})
 app.listen(port, ()=>{
     console.log('Started on port ' + port);
 });
